9192ff247b93ad7fe2b7f66313c0b054649fc716
Guru1990siddayya <47083443+Guru1990siddayya@users.noreply.github.com>
2019-01-30 10:51:33 +0530

    Update CreateTables.sql

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -1,4 +1,4 @@
- create table Risk  
+create table Risk1  
 (  
 RiskId int identity(1,1) primary key,  RiskName varchar(100)
 ); 
@@ -8,16 +8,16 @@ create table gridview
 sid int identity(1,1) primary key,  
 name varchar(100),  
 address varchar(200),
-RiskId int foreign key references Risk(RiskId)
+RiskId int foreign key references Risk1(RiskId)
 );  
  
+insert into Risk1 (RiskName) values('Risk 1')  
+insert into Risk1 (RiskName) values('Risk 2')  
+insert into Risk1 (RiskName) values('Risk 3')  
+  
 insert into gridview (name,address,RiskId) values('Mukesh Kumar','New Delhi',1)  
 insert into gridview (name,address,RiskId) values('Deepa kumar','Utra Khand',2)  
 insert into gridview (name,address,RiskId) values('Pankaj Kumar','New Delhi',3)  
 insert into gridview (name,address,RiskId) values('Sanjeev Kumar','New Delhi',1)  
 insert into gridview (name,address,RiskId) values('Pawan Kumar','New Delhi',2) 
 
-insert into Risk (RiskName) values('Risk 1')  
-insert into Risk (RiskName) values('Risk 2')  
-insert into Risk (RiskName) values('Risk 3')  
-  
